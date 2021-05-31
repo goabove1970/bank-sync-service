e7c99eb6dc5ccc535559e18dda7f4cfcfa35fea2
goabove1970 <goabove1970@gmail.com>
2021-05-30 21:22:39 -0400

    Added listening address to console

--- bin/www.ts
+++ bin/www.ts
@@ -1,16 +1,16 @@
-import { app } from '../app';
+import { app } from "../app";
 
-var debug = require('debug')('api:server');
-var http = require('http');
+var debug = require("debug")("api:server");
+var http = require("http");
 
-var port = normalizePort(process.env.PORT || '9300');
-app.set('port', port);
+var port = normalizePort(process.env.PORT || "9300");
+app.set("port", port);
 
 var server = http.createServer(app);
 
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
 
 function normalizePort(val) {
   var port = parseInt(val, 10);
@@ -27,19 +27,19 @@ function normalizePort(val) {
 }
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
@@ -49,6 +49,7 @@ function onError(error) {
 
 function onListening() {
   var addr = server.address();
-  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  console.log(`Listening on ${bind}`);
+  debug("Listening on " + bind);
 }
