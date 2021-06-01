a165b7247c55f542ac5bd9589b9c3143e1bb5a18
goabove1970 <goabove1970@gmail.com>
2021-06-01 17:51:03 -0400

    Set pocket size to 50 mb

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ var express = require("express");
 import * as path from "path";
 var cors = require("cors");
 var createError = require("http-errors");
+var bodyParser = require("body-parser");
 require("@src/controllers/scheduler");
 
 import * as bankConnectionsRouter from "@root/src/routes/connections-router";
@@ -12,8 +13,10 @@ import { BankAdaptorBase } from "./src/models/bank-adaptor-base";
 export const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/bank-connections", bankConnectionsRouter);
